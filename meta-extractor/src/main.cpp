29d9d3aa9a3ee63dd72c7715d36b3e5e02d3ff6d
Klemens Schölhorn <klemens@schoelhorn.eu>
2015-05-30 18:31:08 +0200

    Implement basic main that reads a warc file and outputs some information

--- meta-extractor/src/main.cpp
+++ meta-extractor/src/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include "WARCReader.h"
+#include "WARCRecord.h"
 
-int main(int, char**) {
-    std::cout << "meta-extractor: Extract meta information from CC WAT files"
-              << std::endl;
+int main(int argc, char** argv) {
+    if(argc != 2) {
+        std::cerr << "meta-extractor: Extract meta information from CC WAT files"
+            << std::endl << "Usage: " << argv[0] << " *warc-file*" << std::endl;
+        return 1;
+    }
+
+    std::ifstream input(argv[1], std::ifstream::binary);
+    if(!input.good()) {
+        std::cerr << "file not found" << std::endl;
+        return 2;
+    }
+
+    WARC::Reader reader(input);
+    WARC::Record<void> record;
+    size_t count = 0;
+    try {
+        while(reader.read(record)) {
+            ++count;
+            std::cout << record.id << ", " << record.date << ", "
+                      << record.type << ", " << record.length << " bytes"
+                      << std::endl;
+
+            // reset record
+            record = WARC::Record<void>();
+        }
+        std::cout << std::endl << count << " records" << std::endl;
+    } catch(const std::exception& e) {
+        std::cerr << "Exception occurred :" << e.what() << std::endl;
+        return 10;
+    }
 }
