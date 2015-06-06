be18c673384f3e997232aaf34ee07921edb65400
Klemens Schölhorn <klemens@schoelhorn.eu>
2015-06-06 17:49:58 +0200

    Add support for files and stdio for input/output
    This is implemented using tclap for command line argument parsing.

--- meta-extractor/src/main.cpp
+++ meta-extractor/src/main.cpp
@@ -2,48 +2,84 @@
 #include <fstream>
 #include "WARCReader.h"
 #include "WARCRecord.h"
+#include "tclap/CmdLine.h"
+
+void processWARC(std::istream&, std::ostream&);
 
 int main(int argc, char** argv) {
-    if(argc != 2) {
-        std::cerr << "meta-extractor: Extract meta information from CC WAT files"
-            << std::endl << "Usage: " << argv[0] << " *warc-file*" << std::endl;
+    std::istream* input;
+    std::ostream* output;
+
+    TCLAP::CmdLine cmd("meta-extractor", ' ', "0.1");
+    TCLAP::ValueArg<std::string> inFile("i", "input", "Input file", false, "", "file", cmd);
+    TCLAP::ValueArg<std::string> outFile("o", "output", "Output file", false, "", "file", cmd);
+    try {
+        cmd.parse(argc, argv);
+    } catch(TCLAP::ArgException &e) {
+        std::cerr << "command line parsing error: " << e.what() << std::endl;
         return 1;
     }
 
-    std::ifstream input(argv[1], std::ifstream::binary);
-    if(!input.good()) {
-        std::cerr << "file not found" << std::endl;
-        return 2;
+    std::ifstream inFileStream;
+    if(inFile.isSet()) {
+        inFileStream.open(inFile.getValue(), std::ifstream::binary);
+        if(!inFileStream.good()) {
+            std::cerr << "input file not found" << std::endl;
+            return 2;
+        }
+        input = &inFileStream;
+    } else {
+        input = &std::cin;
     }
 
+    std::ofstream outFileStream;
+    if(outFile.isSet()) {
+        outFileStream.open(outFile.getValue(), std::ifstream::binary);
+        if(!outFileStream.good()) {
+            std::cerr << "output file not found" << std::endl;
+            return 3;
+        }
+        output = &outFileStream;
+    } else {
+        output = &std::cout;
+    }
+
+    try {
+        processWARC(*input, *output);
+    } catch(const std::exception& e) {
+        std::cerr << "Exception occurred: " << e.what() << std::endl;
+        return 10;
+    }
+
+    return 0;
+}
+
+void processWARC(std::istream& input, std::ostream& output) {
     WARC::Reader reader(input);
     WARC::Record<rapidjson::Document> record;
     size_t count = 0;
     uint64_t maxLength = 0;
-    try {
-        while(reader.read(record)) {
-            // this is not a json record
-            if(!record.valid) {
-                record.clear();
-                continue;
-            }
 
-            ++count;
-            maxLength = std::max(maxLength, record.length);
-            std::cout << record.id << ", " << record.date << ", "
-                      << record.length << " bytes, ";
-
-            std::cout << record.content["Envelope"]["WARC-Header-Metadata"]
-                                       ["Content-Type"].GetString()
-                      << std::endl;
-
-            // clear record because it is reused
+    while(reader.read(record)) {
+        // this is not a json record
+        if(!record.valid) {
             record.clear();
+            continue;
         }
-        std::cout << std::endl << count << " records, max "
-                  << maxLength << " bytes" << std::endl;
-    } catch(const std::exception& e) {
-        std::cerr << "Exception occurred :" << e.what() << std::endl;
-        return 10;
+
+        ++count;
+        maxLength = std::max(maxLength, record.length);
+        output << record.id << ", " << record.date << ", "
+               << record.length << " bytes, ";
+
+        output << record.content["Envelope"]
+                                ["WARC-Header-Metadata"]
+                                ["Content-Type"].GetString()
+               << std::endl;
+
+        // clear record because it is reused
+        record.clear();
     }
+
+    std::cerr << count << " records, max " << maxLength << " bytes" << std::endl;
 }
