0289ec4c4e4dee603973880199e47b135b22e014
Querela <ek.92@arcor.de>
2015-06-12 00:04:51 +0200

    Test some more extractions ...
    Is Try-Catch better?
    Is HasMember() really working? (Nothing in output but in data?)

--- meta-extractor/src/main.cpp
+++ meta-extractor/src/main.cpp
@@ -106,6 +106,7 @@ void processWARC(std::istream& input, CSV::Writer&, int verbosity) {
         std::string content_type = record.content["Envelope"]
                                                  ["WARC-Header-Metadata"]
                                                  ["Content-Type"].GetString();
+        // TODO: Field --> "WARC-Type" : "response"
 
         if (content_type == "application/http; msgtype=response") {
             ++countProcessed;
@@ -155,6 +156,72 @@ void processWARC(std::istream& input, CSV::Writer&, int verbosity) {
                 } // if
             } // for
             // TODO: other header fields
+
+            const rapidjson::Value& http = record.content["Envelope"]
+                                                         ["Payload-Metadata"]
+                                                         ["HTTP-Response-Metadata"];
+            const rapidjson::Value::ConstMemberIterator& html_iter = http.FindMember("HTML-Metadata");
+            if (html_iter != http.MemberEnd()) {
+                if (verbosity >= highVerbosity) {
+                    std::cerr << "Has >>HTML-Metadata<<" << std::endl;
+                } // if
+                const rapidjson::Value& html = html_iter->value;
+                const rapidjson::Value::ConstMemberIterator& head_iter = html.FindMember("Head");
+                if (head_iter != html.MemberEnd()) {
+                    if (verbosity >= highVerbosity) {
+                        std::cerr << "Has >>Head<<" << std::endl;
+                    } // if
+                    const rapidjson::Value& head = head_iter->value;
+                    const rapidjson::Value::ConstMemberIterator scripts_iter = head.FindMember("Scripts");
+                    if (scripts_iter != head.MemberEnd()) {
+                        if (verbosity >= highVerbosity) {
+                            std::cerr << "Has >>Scripts<<" << std::endl;
+                        } // if
+                        const rapidjson::Value& scripts = scripts_iter->value;
+                        for (rapidjson::SizeType i = 0; i < scripts.Size(); ++i) {
+                            std::string script = scripts[i]["url"].GetString();
+                            // TODO: compare to list of popular scripts
+                        } // for
+                        if (verbosity >= highVerbosity) {
+                            std::cerr << "#Scripts: " << scripts.Size() << std::endl;
+                        } // if
+                    } else {
+                        if (verbosity >= highVerbosity) {
+                            std::cerr << "Has no >>Scripts<<" << std::endl;
+                        } // if
+                    } // if
+
+                    const rapidjson::Value::ConstMemberIterator metas_iter = head.FindMember("Metas");
+                    if (metas_iter != head.MemberEnd()) {
+                        if (verbosity >= highVerbosity) {
+                            std::cerr << "Has >>Metas<<" << std::endl;
+                        } // if
+                        const rapidjson::Value& metas = metas_iter->value;
+                        for (rapidjson::SizeType i = 0; i < metas.Size(); ++i) {
+                            const rapidjson::Value& meta = metas[i];
+                            // TODO: check if "HasMember()" is really working!!!
+                            if (meta.HasMember("content-type")) {
+                                std::string ctype_encoding = meta["content"].GetString();
+                                if (verbosity >= highVerbosity) {
+                                    std::cerr << "Encoding/CT: " << ctype_encoding << std::endl;
+                                } // if
+                            } // if
+                        } // for
+                    } else {
+                        if (verbosity >= highVerbosity) {
+                            std::cerr << "Has no >>Metas<<" << std::endl;
+                        } // if
+                    } // if
+                } else {
+                    if (verbosity >= highVerbosity) {
+                        std::cerr << "Has no >>Head<<" << std::endl;
+                    } // if
+                } // if
+            } else {
+                if (verbosity >= highVerbosity) {
+                    std::cerr << "Has no >>HTML-Metadata<<" << std::endl;
+                } // if
+            } // if
 
         } else {
             ++countIgnored;
