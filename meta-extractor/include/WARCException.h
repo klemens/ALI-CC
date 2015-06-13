fa4af8c130e24ecc29c1db94ed664fac6d598c70
Klemens Schölhorn <klemens@schoelhorn.eu>
2015-06-13 18:12:25 +0200

    Add missing import to WARCException.h

--- meta-extractor/include/WARCException.h
+++ meta-extractor/include/WARCException.h
@@ -1,6 +1,8 @@
 #ifndef _WARCEXCEPTION_H_
 #define _WARCEXCEPTION_H_
 
+#include <stdexcept>
+
 namespace WARC {
     class Exception : public std::runtime_error {
         using std::runtime_error::runtime_error;
