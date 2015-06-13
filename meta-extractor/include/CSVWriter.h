d4cd3990499d349f671b5dcd6b2b8dd2e6a47f39
Klemens Schölhorn <klemens@schoelhorn.eu>
2015-06-13 13:52:25 +0200

    Improve and document CSV::Writer interface
    ',' is now the default value separator as in "comma-separated values".

--- meta-extractor/include/CSVWriter.h
+++ meta-extractor/include/CSVWriter.h
@@ -8,19 +8,32 @@
 namespace CSV {
     class Writer {
         public:
-            static const char CSV_SEP = '|';
+            static const char CSV_SEP = ',';
 
+            /**
+             * Construct a CSV::Writer that uses ',' as value separator.
+             */
             Writer(std::ostream& output);
 
+            /**
+             * Write the given value to the csv file
+             */
             Writer operator<< (const std::string& data);
-
+            /**
+             * Advance to the next line
+             */
             Writer next();
+
+        protected:
+            /**
+             * Clean up (escape) the given value to be used in this object.
+             * Note: This is called internally in operator<<(const std::string&)
+             */
+            std::string clean(const std::string& data);
 
         private:
             std::ostream& output;
             bool isFirstValue;
-
-            std::string clean(const std::string&);
     };
 }
 
